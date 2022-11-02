b45d0f02eab316ca9494b36f7603e815ec18c61f
bonghee <hae8064@naver.com>
2022-11-02 18:08:53 +0900

    input auto focus 설정 & select 이외 부분 background 어둡게 변경

--- src/components/Common/Header/HeaderSearch.jsx
+++ src/components/Common/Header/HeaderSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './HeaderSearch.css';
 import { ReactComponent as SearchIcon2 } from '../../../assets/icon-search2.svg';
@@ -6,6 +6,8 @@ import { ReactComponent as SearchIcon2 } from '../../../assets/icon-search2.svg'
 const HeaderSearch = ({ on, changeSearchOn }) => {
   const [searchView, setSearchView] = useState(on);
   let searchRef = useRef(null);
+  const inputRef = useRef(null);
+
   // 검색 리스트 렌더링용 (true이면 보여준다)
   let [inputFocus, setInputFocus] = useState(false);
 
@@ -20,67 +22,78 @@ const HeaderSearch = ({ on, changeSearchOn }) => {
         changeSearchOn();
         console.log('close');
       }
+      searchRef.current.focus();
     }
+    searchRef.current.focus();
+
     document.addEventListener('mousedown', handleOutside);
     return () => {
       document.removeEventListener('mousedown', handleOutside);
     };
   }, [searchRef, searchView]);
 
+  useLayoutEffect(() => {
+    if (searchRef.current !== null) searchRef.current.focus();
+  });
+
   return (
-    <div ref={searchRef} className={'searchBarContainer' + on}>
+    <div className={'searchBarContainer' + on}>
       {/* <div
       ref={searchRef}
       className={
         searchView === 'on' ? 'searchBarContaineron' : 'searchBarContaineroff'
       }
     > */}
-      <div className="formSearch">
-        <form action="">
-          <SearchIcon2 className="searchIconInput" />
-          <input
-            type="search"
-            placeholder="#태그, 회사, 포지션 검색"
-            autoComplete="off"
-          />
-        </form>
-      </div>
+      <div className="searchInputWhite">
+        <div className="formSearch">
+          <form action="">
+            <SearchIcon2 className="searchIconInput" />
+            <input
+              ref={searchRef}
+              type="search"
+              placeholder="#태그, 회사, 포지션 검색"
+              autoComplete="off"
+            />
+          </form>
+        </div>
 
-      <div className="tagContainer">
-        <div className="tagTop">
-          <div className="tagTopLeft">추천태그로 검색해보세요</div>
-          <Link to="/" style={{ textDecoration: 'none' }}>
-            <div className="tagTopRight">
-              기업태그 홈 이동하기
-              <svg width="12" height="12" viewBox="0 0 12 12">
-                <path
-                  fill="currentColor"
-                  d="M4.22 9.72a.75.75 0 001.06 1.06l4.25-4.25a.75.75 0 000-1.06L5.28 1.22a.75.75 0 00-1.06 1.06L7.94 6 4.22 9.72z"
-                ></path>
-              </svg>
-            </div>
-          </Link>
-        </div>
-        <div className="tagBottom">
-          <ul className="tagUl">
-            <li className="tagListItem">
-              <button className="tagListButton1">#퇴사율5%이하</button>
-            </li>
-            <li className="tagListItem">
-              <button className="tagListButton2">#어학교육</button>
-            </li>
-            <li className="tagListItem">
-              <button className="tagListButton3">#핀테크</button>
-            </li>
-            <li className="tagListItem">
-              <button className="tagListButton4">#연봉상위2~5%</button>
-            </li>
-            <li className="tagListItem">
-              <button className="tagListButton5">#수유실</button>
-            </li>
-          </ul>
+        <div className="tagContainer">
+          <div className="tagTop">
+            <div className="tagTopLeft">추천태그로 검색해보세요</div>
+            <Link to="/" style={{ textDecoration: 'none' }}>
+              <div className="tagTopRight">
+                기업태그 홈 이동하기
+                <svg width="12" height="12" viewBox="0 0 12 12">
+                  <path
+                    fill="currentColor"
+                    d="M4.22 9.72a.75.75 0 001.06 1.06l4.25-4.25a.75.75 0 000-1.06L5.28 1.22a.75.75 0 00-1.06 1.06L7.94 6 4.22 9.72z"
+                  ></path>
+                </svg>
+              </div>
+            </Link>
+          </div>
+          <div className="tagBottom">
+            <ul className="tagUl">
+              <li className="tagListItem">
+                <button className="tagListButton1">#퇴사율5%이하</button>
+              </li>
+              <li className="tagListItem">
+                <button className="tagListButton2">#어학교육</button>
+              </li>
+              <li className="tagListItem">
+                <button className="tagListButton3">#핀테크</button>
+              </li>
+              <li className="tagListItem">
+                <button className="tagListButton4">#연봉상위2~5%</button>
+              </li>
+              <li className="tagListItem">
+                <button className="tagListButton5">#수유실</button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+      <div className="searchBlack"></div>
     </div>
   );
 };
