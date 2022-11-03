2c142a7356f07ed589b59c4e6859efec05c55d48
bonghee <hae8064@naver.com>
2022-11-03 13:53:43 +0900

    메인 배너 버튼 생성

---
+++ src/components/Common/TagSlide/TagSlide.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+const TagSlide = () => {
+  return (
+    <div class="headerBottom">
+      <div class="BottomSlickTrack">
+        <button class="manyleftButton">
+          <svg class="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
+            <path d="m6.045 9 5.978-5.977a.563.563 0 1 0-.796-.796L4.852 8.602a.562.562 0 0 0 0 .796l6.375 6.375a.563.563 0 0 0 .796-.796L6.045 9z"></path>
+          </svg>
+        </button>
+        <button class="BottomTrack1">
+          <span class="BottomTrackText">연봉이 최고의 복지</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F634f02e0-9f6e-11ec-b909-0242ac120002.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack2">
+          <span class="BottomTrackText">재택근무</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F5d873f3a-9f6e-11ec-b909-0242ac120002.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack3">
+          <span class="BottomTrackText">퇴사율 10% 이하</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F7d3cdb3c-9f6e-11ec-b909-0242ac120002.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack4">
+          <span class="BottomTrackText">급성장 중</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F37dacf86-9f6e-11ec-b909-0242ac120002.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack5">
+          <span class="BottomTrackText">병역특례</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F6eda33d2-9f6e-11ec-b909-0242ac120002.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack6">
+          <span class="BottomTrackText">50인 이하</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F3a965d18-c524-11ec-901c-acde48001122.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack1">
+          <span class="BottomTrackText">50인 이상</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F4cec3244-c524-11ec-901c-acde48001122.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack1">
+          <span class="BottomTrackText">업력 5년 이상</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F45bb9794-c524-11ec-901c-acde48001122.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="BottomTrack1">
+          <span class="BottomTrackText">유연근무</span>
+          <img
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Ftags%2F827f6146-9f6e-11ec-b909-0242ac120002.png&w=50&q=75"
+            alt=""
+          />
+        </button>
+        <button class="manyRightButton">
+          <svg class="SvgIcon_SvgIcon__root__svg__DKYBi" viewBox="0 0 18 18">
+            <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TagSlide;
--- src/components/MainPage/MainEventBanner/MainEventBanner.jsx
+++ src/components/MainPage/MainEventBanner/MainEventBanner.jsx
@@ -46,6 +46,9 @@ function MainEventBanner() {
         </div>
       </div> */}
 
+      <svg class="mainSlideSvgLeft" viewBox="0 0 18 18">
+        <path d="m6.045 9 5.978-5.977a.563.563 0 1 0-.796-.796L4.852 8.602a.562.562 0 0 0 0 .796l6.375 6.375a.563.563 0 0 0 .796-.796L6.045 9z"></path>
+      </svg>
       <Slider {...settings}>
         <div>
           <img
@@ -68,6 +71,9 @@ function MainEventBanner() {
           />
         </div>
       </Slider>
+      <svg class="mainSlideSvgRight" viewBox="0 0 18 18">
+        <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+      </svg>
     </div>
   );
 }
