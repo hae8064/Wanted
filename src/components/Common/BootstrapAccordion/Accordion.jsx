b00fca503cd964e63198f224c5e35c35da9c8513
bonghee <hae8064@naver.com>
2022-11-16 00:09:37 +0900

    부트스트랩 아코디언 추가

--- src/components/Common/BootstrapAccordion/Accordion.jsx
+++ src/components/Common/BootstrapAccordion/Accordion.jsx
@@ -1,9 +1,13 @@
 import Accordion from 'react-bootstrap/Accordion';
+import './Accordion.css';
 
 function BasicExample() {
   return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
+    <Accordion
+      defaultActiveKey="0"
+      style={{ width: '700px', marginTop: '20px' }}
+    >
+      <Accordion.Item clsasName="accordionContainer" eventKey="0">
         <Accordion.Header>
           <svg
             className="bottomBanner2svg"
@@ -23,21 +27,23 @@ function BasicExample() {
               ></path>
             </g>
           </svg>
-          본 채용정보는 원티드랩의 동의없이 무단전재, 재배포, 재가공할 수
-          없으며, 구직활동 이외의 <br />
-          용도로 사용할 수 없습니다.
+          <span className="accordionSpan1">
+            본 채용정보는 원티드랩의 동의없이 무단전재, 재배포, 재가공할 수
+            없으며, <br /> 구직활동 이외의 용도로 사용할 수 없습니다.
+          </span>
         </Accordion.Header>
-        <Accordion.Body>
-          본 채용 정보는 쿼타랩(쿼타북)에서 제공한 자료를 바탕으로 원티드랩에서
-          표현을 수정하고 이의 배열 및 구성을 편집하여 완성한 원티드랩의
-          저작자산이자 영업자산입니다. 본 정보 및 데이터베이스의 일부 내지는
-          전부에 대하여 원티드랩의 동의 없이 무단전재 또는 재배포, 재가공 및
-          크롤링할 수 없으며, 게재된 채용기업의 정보는 구직자의 구직활동 이외의
-          용도로 사용될 수 없습니다. 원티드랩은 쿼타랩(쿼타북)에서 게재한 자료에
-          대한 오류나 그 밖에 원티드랩이 가공하지 않은 정보의 내용상 문제에
-          대하여 어떠한 보장도 하지 않으며, 사용자가 이를 신뢰하여 취한 조치에
-          대해 책임을 지지 않습니다. &lt;저작권자 (주)원티드랩.
-          무단전재-재배포금지&gt;
+        <Accordion.Body className="accordionSpan2">
+          본 채용 정보는 <strong>쿼타랩(쿼타북)</strong>에서 제공한 자료를
+          바탕으로 원티드랩에서 표현을 수정하고 이의 배열 및 구성을 편집하여
+          완성한 원티드랩의 저작자산이자 영업자산입니다. 본 정보 및
+          데이터베이스의 일부 내지는 전부에 대하여 원티드랩의 동의 없이 무단전재
+          또는 재배포, 재가공 및 크롤링할 수 없으며, 게재된 채용기업의 정보는
+          구직자의 구직활동 이외의 용도로 사용될 수 없습니다. 원티드랩은
+          <strong>쿼타랩(쿼타북)</strong>에서 게재한 자료에 대한 오류나 그 밖에
+          원티드랩이 가공하지 않은 정보의 내용상 문제에 대하여 어떠한 보장도
+          하지 않으며, 사용자가 이를 신뢰하여 취한 조치에 대해 책임을 지지
+          않습니다.{' '}
+          <strong>&lt;저작권자 (주)원티드랩. 무단전재-재배포금지&gt;</strong>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
