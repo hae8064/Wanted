34b3cb6103513b2e799e683ed8ad6e46199423fb
bonghee <hae8064@naver.com>
2022-11-21 14:32:59 +0900

    반응형 구현작업 완료

--- src/components/Common/Header/TabletHeader.jsx
+++ src/components/Common/Header/TabletHeader.jsx
@@ -215,6 +215,24 @@ function TabletHeader({
             <div className="searchIconHeader" onClick={() => setSearchOn(true)}>
               <SearchIcon2 />
             </div>
+            {localStorage.getItem("id") === "lbh8064@naver.com" ? (
+              <div className="alarmContainer">
+                <button className="alarmButton">
+                  <AlarmIcon />
+                </button>
+                <span className="Badge_Badge__BJTzL">
+                  <svg className="svgN" width="5" height="5" viewBox="0 0 6 6">
+                    <g fill="#fff" fill-rule="nonzero">
+                      <path
+                        d="M6.647 11L6.647 7.259 6.688 7.259 9.158 11 11 11 11 5 9.353 5 9.353 8.357 9.322 8.357 7.089 5 5 5 5 11z"
+                        transform="translate(-123 -375) translate(20 365) translate(98 5)"
+                      ></path>
+                    </g>
+                  </svg>
+                </span>
+              </div>
+            ) : null}
+
             <button className="MenuTablet">
               <MenuIcon />
             </button>
